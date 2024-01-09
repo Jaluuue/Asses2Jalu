f46abe0b37ec0ec1194a181af315420fa26cfcca
Jaluu erdiansyah <erdi123ansyah@gmail.com>
2024-01-09 17:18:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/assesment2/User.kt
@@ -0,0 +1,4 @@
+package com.example.assesment2
+
+data class User(var name : String,var missCall : String,var missCallTime : String,
+                var phoneNo : String,var country : String,var imageId : Int)
